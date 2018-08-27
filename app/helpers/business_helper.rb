c698c41acef7b7103eb60443b88a3e9df85c7957
Kyle Wiest <kyle.wiest@contractor.cengage.com>
2018-08-27 10:35:22 -0700

    Add links to similar businesses by category

--- app/helpers/business_helper.rb
+++ app/helpers/business_helper.rb
@@ -26,4 +26,16 @@ module BusinessHelper
 
     safe_join links, ', '
   end
+
+  def similar_business_links(business)
+    links = business.categories.map do |category|
+      content_tag :div, class: 'similar-business-category-link' do
+        link_to "#{category} in #{business.address_city}"
+      end
+    end
+
+    content_tag :div, id: 'similar-business-links' do
+      safe_join links
+    end
+  end
 end
